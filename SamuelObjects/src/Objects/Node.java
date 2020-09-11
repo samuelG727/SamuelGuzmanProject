047f16d0f542cc9bd21d2654388123eafe7be8f9
Samuel Guzman <samuelguzmanajala@gmail.com>
2020-09-11 14:38:53 +0200

    ClasesNuevas_main

---
+++ SamuelObjects/src/Objects/Node.java
@@ -0,0 +1,9 @@
+package Objects;
+
+public class Node {
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+    }
+}
---
+++ SamuelObjects/src/Objects/TreeNode.java
@@ -0,0 +1,10 @@
+package Objects;
+
+public class TreeNode {
+    int data;
+    TreeNode left,right;
+    TreeNode(int data){
+        this.data = data;
+        left = right = null;
+    }
+}
